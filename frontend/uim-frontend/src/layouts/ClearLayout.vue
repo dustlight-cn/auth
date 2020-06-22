0ca8a9e96855f8801806bef4db64045299a2e4f4
Hansin1997 <845612500@qq.com>
2020-06-22 12:11:35 +0800

    邮件HTML渲染改用Freemaker

--- frontend/uim-frontend/src/layouts/ClearLayout.vue
+++ frontend/uim-frontend/src/layouts/ClearLayout.vue
@@ -1,78 +1,83 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-header class="bg-cyan text-white" elevated>
       <q-toolbar>
-        <q-toolbar-title>
-          UIM
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn flat round dense icon="assignment_ind"/>
+        <q-toolbar-title>UIM</q-toolbar-title>
+        <q-btn flat round dense icon="sim_card" class="q-mr-xs"/>
+        <q-btn flat round dense icon="gamepad"/>
+      </q-toolbar>
+      <q-toolbar inset>
+        <q-breadcrumbs active-color="white" style="font-size: 16px">
+          <q-breadcrumbs-el label="Home" icon="home"/>
+          <q-breadcrumbs-el label="Components" icon="widgets"/>
+          <q-breadcrumbs-el label="Toolbar"/>
+        </q-breadcrumbs>
       </q-toolbar>
     </q-header>
-
     <q-page-container>
-      <router-view />
+      <router-view/>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink'
+  import EssentialLink from 'components/EssentialLink'
 
-export default {
-  name: 'MainLayout',
+  export default {
+    name: 'MainLayout',
 
-  components: {
-    EssentialLink
-  },
+    components: {
+      EssentialLink
+    },
 
-  data () {
-    return {
-      essentialLinks: [
-        {
-          title: 'Docs',
-          caption: 'quasar.dev',
-          icon: 'school',
-          link: 'https://quasar.dev'
-        },
-        {
-          title: 'Github',
-          caption: 'github.com/quasarframework',
-          icon: 'code',
-          link: 'https://github.com/quasarframework'
-        },
-        {
-          title: 'Discord Chat Channel',
-          caption: 'chat.quasar.dev',
-          icon: 'chat',
-          link: 'https://chat.quasar.dev'
-        },
-        {
-          title: 'Forum',
-          caption: 'forum.quasar.dev',
-          icon: 'record_voice_over',
-          link: 'https://forum.quasar.dev'
-        },
-        {
-          title: 'Twitter',
-          caption: '@quasarframework',
-          icon: 'rss_feed',
-          link: 'https://twitter.quasar.dev'
-        },
-        {
-          title: 'Facebook',
-          caption: '@QuasarFramework',
-          icon: 'public',
-          link: 'https://facebook.quasar.dev'
-        },
-        {
-          title: 'Quasar Awesome',
-          caption: 'Community Quasar projects',
-          icon: 'favorite',
-          link: 'https://awesome.quasar.dev'
-        }
-      ]
+    data() {
+      return {
+        essentialLinks: [
+          {
+            title: 'Docs',
+            caption: 'quasar.dev',
+            icon: 'school',
+            link: 'https://quasar.dev'
+          },
+          {
+            title: 'Github',
+            caption: 'github.com/quasarframework',
+            icon: 'code',
+            link: 'https://github.com/quasarframework'
+          },
+          {
+            title: 'Discord Chat Channel',
+            caption: 'chat.quasar.dev',
+            icon: 'chat',
+            link: 'https://chat.quasar.dev'
+          },
+          {
+            title: 'Forum',
+            caption: 'forum.quasar.dev',
+            icon: 'record_voice_over',
+            link: 'https://forum.quasar.dev'
+          },
+          {
+            title: 'Twitter',
+            caption: '@quasarframework',
+            icon: 'rss_feed',
+            link: 'https://twitter.quasar.dev'
+          },
+          {
+            title: 'Facebook',
+            caption: '@QuasarFramework',
+            icon: 'public',
+            link: 'https://facebook.quasar.dev'
+          },
+          {
+            title: 'Quasar Awesome',
+            caption: 'Community Quasar projects',
+            icon: 'favorite',
+            link: 'https://awesome.quasar.dev'
+          }
+        ]
+      }
     }
   }
-}
 </script>
--- sender/email-core/src/main/java/cn/dustlight/sender/email/EmailSender.java
+++ sender/email-core/src/main/java/cn/dustlight/sender/email/EmailSender.java
@@ -1,32 +1,26 @@
 package cn.dustlight.sender.email;
 
 import cn.dustlight.sender.core.ISender;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.thymeleaf.context.Context;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.templateresolver.ITemplateResolver;
-import org.thymeleaf.templateresolver.StringTemplateResolver;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
 
 public abstract class EmailSender implements ISender {
 
-    @Autowired
-    private SpringTemplateEngine engine;
-
-    private ITemplateResolver templateResolver;
 
     @Override
     public void send(String template, Map<String, String> data, Map<String, Object> parameters, String... receivers) throws IOException {
-        if (templateResolver == null) {
-            templateResolver = new StringTemplateResolver();
-            engine.setTemplateResolver(templateResolver);
+        String content = null;
+        try {
+            content = renderer(template, parameters);
+        } catch (TemplateException e) {
+            throw new IOException(e);
         }
-        Context context = new Context();
-        context.setVariables(parameters);
-        String content = engine.process(template, context);
         String subject = data.getOrDefault("subject", "subject");
         String from = data.getOrDefault("from", null);
         doSend(subject, content, from, receivers);
@@ -36,6 +30,10 @@ public abstract class EmailSender implements ISender {
         Map<String, String> data = new HashMap<>();
         data.put("subject", subject);
         send(template, data, parameters, receivers);
+    }
+
+    protected String renderer(String template, Map<String, ?> parameters) throws IOException, TemplateException {
+        return FreeMarkerTemplateUtils.processTemplateIntoString(new Template("template", new StringReader(template), null), parameters);
     }
 
     protected abstract void doSend(String subject, String content, String from, String... receivers) throws IOException;
