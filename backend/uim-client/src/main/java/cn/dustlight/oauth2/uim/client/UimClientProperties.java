89989512d54d941a3bb9ead7423c55ed1d6198a6
Hansin1997 <845612500@qq.com>
2020-09-10 23:43:52 +0800

    uim-client restful模式

--- backend/uim-client/src/main/java/cn/dustlight/oauth2/uim/client/UimClientProperties.java
+++ backend/uim-client/src/main/java/cn/dustlight/oauth2/uim/client/UimClientProperties.java
@@ -4,15 +4,27 @@ import cn.dustlight.oauth2.uim.client.converter.IUimUserConverter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "dustlight.uim-client")
 public class UimClientProperties {
 
+    /**
+     * 用户信息路径，例如第三方登录返回数据为 {code: 200,data:{user:{username: name,email: email},extend:123}}，则用户信息路径为 data/user
+     */
     private Map<String, String> userDataPath = new LinkedHashMap<>();
+    /**
+     * 自定义用户转换
+     */
     private Map<String, Class<? extends IUimUserConverter>> customConverter = new LinkedHashMap<>();
+    /**
+     * 用户信息映射，用于默认转换
+     */
     private Map<String, UserDetailsMapping> userDetailsMapping = new LinkedHashMap<>();
+    /**
+     * 是否启用Restful模式，启用后登录认证等将不进行重定向改为返回Json。
+     */
+    public boolean restfulEnabled = true;
 
     public Map<String, Class<? extends IUimUserConverter>> getCustomConverter() {
         return customConverter;
@@ -36,6 +48,14 @@ public class UimClientProperties {
 
     public void setUserDetailsMapping(Map<String, UserDetailsMapping> userDetailsMapping) {
         this.userDetailsMapping = userDetailsMapping;
+    }
+
+    public boolean isRestfulEnabled() {
+        return restfulEnabled;
+    }
+
+    public void setRestfulEnabled(boolean restfulEnabled) {
+        this.restfulEnabled = restfulEnabled;
     }
 
     public static class UserDetailsMapping {
--- backend/uim-client/src/main/java/cn/dustlight/oauth2/uim/client/confgurations/UimClientSecurityConfiguration.java
+++ backend/uim-client/src/main/java/cn/dustlight/oauth2/uim/client/confgurations/UimClientSecurityConfiguration.java
@@ -1,25 +1,84 @@
 package cn.dustlight.oauth2.uim.client.confgurations;
 
+import cn.dustlight.oauth2.uim.client.UimClientProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+
+@EnableConfigurationProperties(UimClientProperties.class)
 public class UimClientSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService;
 
+    @Autowired
+    private UimClientProperties properties;
+
+    @Autowired
+    private MappingJackson2HttpMessageConverter converter;
+
+    private static final String CONTENT_TYPE_JSON = "application/json;charset=utf-8";
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated()
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2Client()
                 .and()
                 .oauth2Login()
                 .userInfoEndpoint()
                 .userService(oAuth2UserService);
-        http.oauth2Client();
+        if (properties.isRestfulEnabled()) {
+            http.exceptionHandling()
+                    .authenticationEntryPoint(authenticationEntryPoint())
+                    .accessDeniedHandler(accessDeniedHandler())
+                    .and()
+                    .oauth2Login()
+                    .successHandler(successHandler())
+                    .failureHandler(failHandler());
+        }
     }
+
+    protected AccessDeniedHandler accessDeniedHandler() {
+        return ((httpServletRequest, httpServletResponse, e) -> {
+            httpServletResponse.setStatus(403);
+            httpServletResponse.getWriter().println(e.getMessage());
+        });
+    }
+
+    protected AuthenticationEntryPoint authenticationEntryPoint() {
+        return ((httpServletRequest, httpServletResponse, e) -> {
+            httpServletResponse.setStatus(401);
+            httpServletResponse.getWriter().println(e.getMessage());
+        });
+    }
+
+    protected AuthenticationFailureHandler failHandler() {
+        return ((httpServletRequest, httpServletResponse, e) -> {
+            httpServletResponse.setStatus(400);
+            httpServletResponse.getWriter().println(e.getMessage());
+        });
+    }
+
+    protected AuthenticationSuccessHandler successHandler() {
+        return ((httpServletRequest, httpServletResponse, authentication) -> {
+            httpServletResponse.setStatus(200);
+            httpServletResponse.setContentType(CONTENT_TYPE_JSON);
+            converter.getObjectMapper().writeValue(httpServletResponse.getWriter(), authentication.getPrincipal());
+        });
+    }
+
 
 }
