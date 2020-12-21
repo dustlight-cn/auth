6a5a2da79b541342a5666b43ea5df4fd4c58e107
Hansin1997 <845612500@qq.com>
2020-12-22 00:29:38 +0800

    【重构】 初始化 auth-client-axios 以及 auth-ui

---
+++ auth-ui/README.md
@@ -0,0 +1,22 @@
+# auth-ui (auth-ui)
+
+UI for auth-service
+
+## Install the dependencies
+```bash
+yarn
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.conf.js](https://quasar.dev/quasar-cli/quasar-conf-js).
---
+++ auth-ui/src/boot/auth-client-axios.ts
@@ -0,0 +1,31 @@
+import {
+  UsersApi, TokenApi, AuthorizationApi,
+  CAPTCHAApi
+} from '@dustlight/auth-client-axios'
+import {BASE_PATH} from '@dustlight/auth-client-axios/base'
+import {boot} from "quasar/wrappers";
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $userApi: UsersApi;
+    $tokenApi: TokenApi;
+    $authorizationApi: AuthorizationApi
+    $captchaApi: CAPTCHAApi
+  }
+}
+
+export const basePath = BASE_PATH;
+
+export const userApi = new UsersApi();
+export const tokenApi = new TokenApi();
+export const authorizationApi = new AuthorizationApi();
+export const captchaApi = new CAPTCHAApi();
+
+export default boot(({Vue}) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  Vue.prototype.$userApi = userApi;
+  Vue.prototype.$tokenApi = tokenApi;
+  Vue.prototype.$authorizationApi = authorizationApi;
+  Vue.prototype.$captchaApi = captchaApi;
+});
+
