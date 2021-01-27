2b2957beb63ce87ad47cdd78c52bff8a81997e05
Hansin1997 <845612500@qq.com>
2021-01-27 21:02:41 +0800

    【重构】 应用授权作用域编辑。

---
+++ auth-ui/src/components/NoResults.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-center text-caption text-grey">{{ $t("noResults") }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NoResults"
+}
+</script>
+
+<style scoped>
+
+</style>
