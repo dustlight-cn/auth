65896a6d059153b7c3673ffe224fe115448abf77
Hansin1997 <845612500@qq.com>
2021-02-23 01:59:20 +0800

    【重构】 1、配置文件选择文件存储实现。2、更新 Captcha 版本，不再报字体空指针。

--- auth-service/src/main/java/cn/dustlight/auth/configurations/components/StorageConfiguration.java
+++ auth-service/src/main/java/cn/dustlight/auth/configurations/components/StorageConfiguration.java
@@ -5,13 +5,14 @@ import cn.dustlight.auth.services.storages.CdnStorageProxy;
 import cn.dustlight.storage.core.RestfulStorage;
 import cn.dustlight.storage.core.Storage;
 import cn.dustlight.storage.local.LocalStorage;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -19,29 +20,50 @@ import java.util.HashSet;
 @EnableConfigurationProperties(StorageConfiguration.StorageProperties.class)
 public class StorageConfiguration {
 
+    private Log logger = LogFactory.getLog(StorageConfiguration.class.getName());
+
     @Bean
-    @ConditionalOnMissingBean
-    @Order()
-    public StorageHandler storageHandler(@Autowired BeanFactory beanFactory,
-                                         @Autowired StorageProperties properties,
-                                         @Autowired(required = false) RestfulStorage restfulStorage) {
+    @ConditionalOnProperty(name = "dustlight.auth.storage.storage-type",
+            havingValue = "local", matchIfMissing = true)
+    public StorageHandler localStorageHandler(@Autowired BeanFactory beanFactory,
+                                              @Autowired StorageProperties properties) {
         Collection<Storage> storages = new HashSet<>();
-        if (restfulStorage != null) {
-            CdnStorageProxy storage = new CdnStorageProxy(restfulStorage, properties.getBaseUrl());
-            storage.setPrefix(properties.getPrefix());
-            storage.setSimpleUrl(properties.isSimpleMode());
-            storages.add(storage);
-        }
+
         storages.add(LocalStorage.from((properties.baseUrl != null ? properties.baseUrl : "") +
                 (properties.prefix != null ? properties.prefix : "")));
+
         StorageHandler handler = new StorageHandler(storages);
         handler.setExpiration(properties.getDefaultExpiration());
+        logger.info("Storage Mode: LOCAL");
+        return handler;
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "dustlight.auth.storage.storage-type",
+            havingValue = "storage")
+    public StorageHandler storageHandler(@Autowired BeanFactory beanFactory,
+                                         @Autowired StorageProperties properties,
+                                         @Autowired Storage storage) {
+        Collection<Storage> storages = new HashSet<>();
+        if (storage instanceof RestfulStorage) {
+            RestfulStorage restfulStorage = (RestfulStorage) storage;
+            CdnStorageProxy proxy = new CdnStorageProxy(restfulStorage, properties.getBaseUrl());
+            proxy.setPrefix(properties.getPrefix());
+            proxy.setSimpleUrl(properties.isSimpleMode());
+            storages.add(proxy);
+        } else {
+            storages.add(storage);
+        }
+        StorageHandler handler = new StorageHandler(storages);
+        handler.setExpiration(properties.getDefaultExpiration());
+        logger.info("Storage Mode: STORAGE");
         return handler;
     }
 
     @ConfigurationProperties("dustlight.auth.storage")
     public static class StorageProperties {
 
+        private StorageType storageType = StorageType.LOCAL;
         private boolean simpleMode = true;
         private String baseUrl = null;
         private String prefix = "upload/auth/";
@@ -77,6 +99,19 @@ public class StorageConfiguration {
 
         public void setSimpleMode(boolean simpleMode) {
             this.simpleMode = simpleMode;
+        }
+
+        public StorageType getStorageType() {
+            return storageType;
+        }
+
+        public void setStorageType(StorageType storageType) {
+            this.storageType = storageType;
+        }
+
+        public enum StorageType {
+            LOCAL,
+            STORAGE
         }
     }
 }
