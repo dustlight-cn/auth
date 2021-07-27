279b2fb16281657920621f56300dbc8bc871e8cd
Hansin1997 <845612500@qq.com>
2021-07-27 11:56:39 +0800

    增加 js 客户端

--- auth-client-axios/update.cmd
+++ auth-client-axios/update.cmd
@@ -1,1 +1,2 @@
 openapi-generator-cli generate -i http://localhost:8080/v1/api-doc -g typescript-axios -o .
+tsc --build tsconfig.json
--- auth-client-axios/update.sh
+++ auth-client-axios/update.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
 openapi-generator-cli generate -i http://localhost:8080/v0/api-doc -g typescript-axios -o .
+tsc --build tsconfig.json
